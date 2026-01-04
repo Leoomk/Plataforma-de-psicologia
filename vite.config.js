c7a9657f40ad6bef219ec93d7f1cec7aacdfc065
Leoomk <leo.goncalves@hotmail.com>
2026-01-04 13:39:45 -0300

    Fix base URL in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/plataforma-psicologia/',
+  base: '/Plataforma-de-psicologia/',
   plugins: [react()],
 })
